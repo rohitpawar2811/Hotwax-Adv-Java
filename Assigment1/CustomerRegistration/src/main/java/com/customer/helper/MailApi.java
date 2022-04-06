c743eb567b2787b2d75617f1a11ea04030620e65
Rohit pawar <rohitpawar28112000@gmail.com>
2022-04-06 15:16:40 +0530

    mail.properties file created in which user,password is stored of the Sender

--- Assigment1/CustomerRegistration/src/main/java/com/customer/helper/MailApi.java
+++ Assigment1/CustomerRegistration/src/main/java/com/customer/helper/MailApi.java
@@ -4,10 +4,11 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class MailApi {
 
-    public static String from = "rohitpawar28112000@gmail.com";
+
 
     public static boolean sendMail(String msg , String subject , String to){
         boolean b=false;
@@ -25,11 +26,16 @@ public class MailApi {
         getPropertie.put("mail.smtp.ssl.enable","true");
         getPropertie.put("mail.smtp.auth","true");
 
+        ResourceBundle mailProperties = ResourceBundle.getBundle("mail");
+        final String from = mailProperties.getString("user");
+        final String password = mailProperties.getString("password");
+
+
         // getting Session or we can say creating
         Session session=Session.getInstance(getPropertie, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from,"@Rohit28112000");
+                return new PasswordAuthentication(from,password);
             }
         });
         //to get error on the console so that we can check it
